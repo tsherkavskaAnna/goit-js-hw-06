fce026b76f58d5b0aa85907f2a72927245318f08
annatsherkavska <katotoka1985@gmail.com>
2023-06-14 17:00:19 +0200

    done task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,26 +1,38 @@
+const input = document.querySelector('input')
+const btnCreate = document.querySelector('button[data-create]')
+const btnDestroy = document.querySelector('button[data-destroy]')
+const box = document.getElementById('boxes')
+
+
+btnCreate.addEventListener('click', onCreateBtn);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(number) {
+  const numberOfBoxes = parseInt(input.value);
+  let size = 30;
+
+  for(let i = 0; i < numberOfBoxes; i++) {
+    const element = document.createElement('div');
+    size += 10;
+    element.style.backgroundColor = getRandomHexColor();
+    element.classList.add('boxEl');
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+    box.appendChild(element);
+  }
+}
+
+function onCreateBtn() {
+  createBoxes()
+}
+
+function destroyBoxes() {
+  box.innerHTML = ""
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
-
-
-
-// const people = [
-//     {
-//         name: "Alex",
-//         know: ["Alex", "John"],
-//     },
-//     {
-//         name: "John",
-//         know: [],
-//     },{
-//         name: "Eva",
-//         know: ["Alex", "John"],
-//     },{
-//         name: "Ivan",
-//         know: ["John", "Eva"],
-//     },
-// ]
-// console.log(people)
 
